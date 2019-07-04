c2bf7ba41d73838fcdb6cc8131ab961528ccb23f
Matthias Pigulla <mp@webfactory.de>
2019-07-04 21:33:23 +0000

    Some brush-ups, tweak Travis build matrix, update PHPUnit, drop webfactory/symfony-application-tests dependency

--- DependencyInjection/WebfactorySlugValidationExtension.php
+++ DependencyInjection/WebfactorySlugValidationExtension.php
@@ -7,11 +7,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration.
- *
- * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
- */
 class WebfactorySlugValidationExtension extends Extension
 {
     /**
@@ -19,9 +14,6 @@ class WebfactorySlugValidationExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
